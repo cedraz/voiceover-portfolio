e9beda88402f261a69ebf9a68dab3d11456279cf
Ícaro Cedraz <icarocedraz7@gmail.com>
2023-09-30 01:13:01 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
