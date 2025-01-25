713a815f077f7754b899b1c6bae44d62c045b43d
cedraz <icarocedraz7@gmail.com>
2025-01-25 20:00:55 -0300

    change phone number

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,230 +1,230 @@
-'use client'
-import * as React from 'react'
+'use client';
+import * as React from 'react';
 import {
-    Typography,
-    Box,
-    Grid,
-    Divider,
-    Link,
-    Stack,
-    IconButton,
-} from '@mui/material'
-import MicIcon from '@mui/icons-material/Mic'
-import WhatsAppIcon from '@mui/icons-material/WhatsApp'
-import EmailIcon from '@mui/icons-material/Email'
-import GitHubIcon from '@mui/icons-material/GitHub'
-import { useTheme } from '@mui/material/styles'
+  Typography,
+  Box,
+  Grid,
+  Divider,
+  Link,
+  Stack,
+  IconButton,
+} from '@mui/material';
+import MicIcon from '@mui/icons-material/Mic';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { useTheme } from '@mui/material/styles';
 
 export default function Footer() {
-    const theme = useTheme()
+  const theme = useTheme();
 
-    const links = [
-        {
-            id: 'Início',
-            href: '#Início',
-        },
-        {
-            id: 'Categorias',
-            href: '#Categorias',
-        },
-        {
-            id: 'Portfólio',
-            href: '#Portfólio',
-        },
-        {
-            id: 'Sobre',
-            href: '#Sobre',
-        },
-        {
-            id: 'Feedbacks',
-            href: '#Feedbacks',
-        },
-    ]
+  const links = [
+    {
+      id: 'Início',
+      href: '#Início',
+    },
+    {
+      id: 'Categorias',
+      href: '#Categorias',
+    },
+    {
+      id: 'Portfólio',
+      href: '#Portfólio',
+    },
+    {
+      id: 'Sobre',
+      href: '#Sobre',
+    },
+    {
+      id: 'Feedbacks',
+      href: '#Feedbacks',
+    },
+  ];
 
-    return (
-        <>
-            <Box
-                sx={{
-                    backgroundColor: theme.footer.bgcolor,
-                    width: '100%',
-                    height: 'fit-content',
-                    p: {
-                        xs: '30px 30px 25px 30px',
-                    },
-                    color: theme.footer.color.primary,
-                }}
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: theme.footer.bgcolor,
+          width: '100%',
+          height: 'fit-content',
+          p: {
+            xs: '30px 30px 25px 30px',
+          },
+          color: theme.footer.color.primary,
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            mb: '10px',
+            alignItems: 'center',
+            justifyContent: {
+              xs: 'center',
+              md: 'space-between',
+              lg: 'space-around',
+            },
+          }}
+        >
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: {
+                xs: 'center',
+                sm: 'flex-start',
+              },
+            }}
+          >
+            <IconButton color="primary" href="#Início" sx={{ p: '8px 0' }}>
+              <MicIcon sx={{ fontSize: 36 }} />
+            </IconButton>
+            <Typography
+              variant="subtitle1"
+              color={theme.footer.color.primary}
+              sx={{ cursor: 'pointer' }}
             >
-                <Grid
-                    container
-                    spacing={2}
-                    sx={{
-                        mb: '10px',
-                        alignItems: 'center',
-                        justifyContent: {
-                            xs: 'center',
-                            md: 'space-between',
-                            lg: 'space-around',
-                        },
-                    }}
-                >
-                    <Grid
-                        item
-                        xs={12}
-                        sm={4}
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: {
-                                xs: 'center',
-                                sm: 'flex-start',
-                            },
-                        }}
-                    >
-                        <IconButton color="primary" href="#Início" sx={{ p: '8px 0' }}>
-                            <MicIcon sx={{ fontSize: 36 }} />
-                        </IconButton>
-                        <Typography
-                            variant="subtitle1"
-                            color={theme.footer.color.primary}
-                            sx={{ cursor: 'pointer' }}
-                        >
               Geovana Alves
-                        </Typography>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        sm={4}
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Typography variant="subtitle2" sx={{ textAlign: 'center' }}>
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography variant="subtitle2" sx={{ textAlign: 'center' }}>
               © 2022 - 2023 Geovana Alves
-                        </Typography>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        sm={4}
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: {
-                                xs: 'flex-start',
-                                mm: 'center',
-                                sm: 'flex-end',
-                            },
-                        }}
-                    >
-                        <Stack
-                            direction="row"
-                            spacing={3}
-                            sx={{ fontSize: '12px', mt: '10px', mb: '10px' }}
-                        >
-                            <IconButton
-                                target="_blank"
-                                href={
-                                    'https://api.whatsapp.com/send?phone=5571981564988&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20minha%20locu%C3%A7%C3%A3o.'
-                                }
-                                sx={{ p: '0' }}
-                            >
-                                <WhatsAppIcon
-                                    sx={{
-                                        fontSize: 25,
-                                        color: theme.footer.color.primary,
-                                        ':hover': {
-                                            color: '#0dc043',
-                                        },
-                                    }}
-                                />
-                            </IconButton>
-                            <IconButton
-                                target="_blank"
-                                href={
-                                    'mailto:geovana.alvesdeoli@gmail.com?subject=Pedido%20de%20orçamento%20para%20locução&body=Olá%2C%20gostaria%20de%20solicitar%20um%20orçamento%20para%20minha%20locução'
-                                }
-                                sx={{ p: '0' }}
-                            >
-                                <EmailIcon
-                                    sx={{
-                                        fontSize: 25,
-                                        color: theme.footer.color.primary,
-                                        ':hover': {
-                                            color: '#ce3c30',
-                                        },
-                                    }}
-                                />
-                            </IconButton>
-                        </Stack>
-                    </Grid>
-                </Grid>
-                <Box
-                    sx={{
-                        mb: '25px',
-                        display: 'flex',
-                        justifyContent: {
-                            xs: 'flex-start',
-                            mm: 'center',
-                        },
-                    }}
-                >
-                    <Stack
-                        sx={{
-                            flexWrap: 'wrap',
-                            flexDirection: {
-                                xs: 'column',
-                                mm: 'row',
-                            },
-                            gap: {
-                                xs: '5px',
-                                mm: '15px',
-                            },
-                        }}
-                    >
-                        {links.map(({ id, href }) => (
-                            <Link
-                                key={id}
-                                variant="body2"
-                                href={href}
-                                sx={{ color: theme.footer.color.secondary }}
-                                underline="hover"
-                            >
-                                {id}
-                            </Link>
-                        ))}
-                    </Stack>
-                </Box>
-                <Divider
-                    variant="middle"
-                    sx={{
-                        backgroundColor: theme.footer.divider,
-                        m: {
-                            xs: '0 0',
-                        },
-                    }}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: {
+                xs: 'flex-start',
+                mm: 'center',
+                sm: 'flex-end',
+              },
+            }}
+          >
+            <Stack
+              direction="row"
+              spacing={3}
+              sx={{ fontSize: '12px', mt: '10px', mb: '10px' }}
+            >
+              <IconButton
+                target="_blank"
+                href={
+                  'https://api.whatsapp.com/send?phone=5571988078997&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20minha%20locu%C3%A7%C3%A3o.'
+                }
+                sx={{ p: '0' }}
+              >
+                <WhatsAppIcon
+                  sx={{
+                    fontSize: 25,
+                    color: theme.footer.color.primary,
+                    ':hover': {
+                      color: '#0dc043',
+                    },
+                  }}
                 />
+              </IconButton>
+              <IconButton
+                target="_blank"
+                href={
+                  'mailto:geovana.alvesdeoli@gmail.com?subject=Pedido%20de%20orçamento%20para%20locução&body=Olá%2C%20gostaria%20de%20solicitar%20um%20orçamento%20para%20minha%20locução'
+                }
+                sx={{ p: '0' }}
+              >
+                <EmailIcon
+                  sx={{
+                    fontSize: 25,
+                    color: theme.footer.color.primary,
+                    ':hover': {
+                      color: '#ce3c30',
+                    },
+                  }}
+                />
+              </IconButton>
+            </Stack>
+          </Grid>
+        </Grid>
+        <Box
+          sx={{
+            mb: '25px',
+            display: 'flex',
+            justifyContent: {
+              xs: 'flex-start',
+              mm: 'center',
+            },
+          }}
+        >
+          <Stack
+            sx={{
+              flexWrap: 'wrap',
+              flexDirection: {
+                xs: 'column',
+                mm: 'row',
+              },
+              gap: {
+                xs: '5px',
+                mm: '15px',
+              },
+            }}
+          >
+            {links.map(({ id, href }) => (
+              <Link
+                key={id}
+                variant="body2"
+                href={href}
+                sx={{ color: theme.footer.color.secondary }}
+                underline="hover"
+              >
+                {id}
+              </Link>
+            ))}
+          </Stack>
+        </Box>
+        <Divider
+          variant="middle"
+          sx={{
+            backgroundColor: theme.footer.divider,
+            m: {
+              xs: '0 0',
+            },
+          }}
+        />
 
-                <Box sx={{ mt: '15px', display: 'flex', justifyContent: 'center' }}>
-                    <Typography variant="subtitle2">
+        <Box sx={{ mt: '15px', display: 'flex', justifyContent: 'center' }}>
+          <Typography variant="subtitle2">
             Criado por{' '}
-                        <Link
-                            variant="body2"
-                            target="_blank"
-                            href="https://github.com/cedraz"
-                            sx={{ color: theme.footer.color.primary }}
-                            underline="hover"
-                        >
+            <Link
+              variant="body2"
+              target="_blank"
+              href="https://github.com/cedraz"
+              sx={{ color: theme.footer.color.primary }}
+              underline="hover"
+            >
               cedraz
-                            <IconButton sx={{ color: theme.footer.color.primary }}>
-                                <GitHubIcon></GitHubIcon>
-                            </IconButton>
-                        </Link>
-                    </Typography>
-                </Box>
-            </Box>
-        </>
-    )
+              <IconButton sx={{ color: theme.footer.color.primary }}>
+                <GitHubIcon></GitHubIcon>
+              </IconButton>
+            </Link>
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,146 +1,148 @@
-'use client'
-import * as React from 'react'
-import Image from 'next/image'
-import { Grid, Box, Typography, Button } from '@mui/material'
-import { useTheme } from '@mui/material/styles'
+'use client';
+import * as React from 'react';
+import Image from 'next/image';
+import { Grid, Box, Typography, Button } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-import heroImage from '../../../public/images/hero.png'
+import heroImage from '../../../public/images/hero.png';
 
 export default function Hero() {
-    const theme = useTheme()
+  const theme = useTheme();
 
-    return (
+  return (
+    <Grid
+      id={'Início'}
+      container
+      className="image-container"
+      sx={{
+        height: {
+          xs: 'fit-content',
+        },
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: theme.palette.background.default,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            md: 'row',
+          },
+          marginTop: {
+            xs: '100px',
+            md: '140px',
+          },
+          width: {
+            xs: '100%',
+          },
+          padding: {
+            xs: '0 20px',
+            sm: '0 40px',
+            md: '0 100px',
+            lg: '0 120px',
+            xl: '0 180px',
+          },
+        }}
+      >
         <Grid
-            id={'Início'}
-            container
-            className="image-container"
-            sx={{
-                height: {
-                    xs: 'fit-content',
-                },
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: theme.palette.background.default,
-            }}
+          sx={{
+            width: {
+              xs: '100%',
+            },
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: {
+              xs: 'center',
+              md: 'center',
+            },
+            alignItems: {
+              xs: 'center',
+              md: 'flex-start',
+            },
+            marginBottom: {
+              xs: '40px',
+            },
+          }}
         >
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: {
-                        xs: 'column',
-                        md: 'row',
-                    },
-                    marginTop: {
-                        xs: '100px',
-                        md: '140px',
-                    },
-                    width: {
-                        xs: '100%',
-                    },
-                    padding: {
-                        xs: '0 20px',
-                        sm: '0 40px',
-                        md: '0 100px',
-                        lg: '0 120px',
-                        xl: '0 180px',
-                    },
-                }}
-            >
-                <Grid
-                    sx={{
-                        width: {
-                            xs: '100%',
-                        },
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: {
-                            xs: 'center',
-                            md: 'center',
-                        },
-                        alignItems: {
-                            xs: 'center',
-                            md: 'flex-start',
-                        },
-                        marginBottom: {
-                            xs: '40px',
-                        },
-                    }}
-                >
-                    <Typography
-                        variant="h3"
-                        sx={{
-                            marginBottom: '15px',
-                            fontSize: {
-                                xs: '1.87rem',
-                                md: '2.5rem',
-                                lg: '3rem',
-                            },
-                            textAlign: {
-                                xs: 'center',
-                                md: 'left',
-                            },
-                            paddingRight: {
-                                xs: '0',
-                                sm: '0',
-                                md: '0px',
-                                lg: '80px',
-                                xl: '100px',
-                            },
-                        }}
-                    >
+          <Typography
+            variant="h3"
+            sx={{
+              marginBottom: '15px',
+              fontSize: {
+                xs: '1.87rem',
+                md: '2.5rem',
+                lg: '3rem',
+              },
+              textAlign: {
+                xs: 'center',
+                md: 'left',
+              },
+              paddingRight: {
+                xs: '0',
+                sm: '0',
+                md: '0px',
+                lg: '80px',
+                xl: '100px',
+              },
+            }}
+          >
             Olá, tudo bem? Eu sou a Locutora Geovana Alves
-                    </Typography>
-                    <Typography
-                        variant="body1"
-                        sx={{
-                            marginBottom: {
-                                xs: '50px',
-                                sm: '30px',
-                            },
-                            color: theme.palette.text.secondary,
-                            textAlign: {
-                                xs: 'center',
-                                md: 'left',
-                            },
-                            paddingRight: {
-                                xs: '0',
-                                sm: '0',
-                                md: '30px',
-                                lg: '40px',
-                                xl: '60px',
-                            },
-                        }}
-                    >
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              marginBottom: {
+                xs: '50px',
+                sm: '30px',
+              },
+              color: theme.palette.text.secondary,
+              textAlign: {
+                xs: 'center',
+                md: 'left',
+              },
+              paddingRight: {
+                xs: '0',
+                sm: '0',
+                md: '30px',
+                lg: '40px',
+                xl: '60px',
+              },
+            }}
+          >
             Aqui você terá acesso ao meu portfólio de locução, à descrição dos
             meus serviços e aos meus contatos. Além disso, você pode solicitar o
             seu orçamento. Vou me empenhar para que a locução fique perfeita
             para o que você precisa!
-                    </Typography>
-                    <Button
-                        href={'https://api.whatsapp.com/send?phone=5571981564988&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20minha%20locu%C3%A7%C3%A3o.'}
-                        variant="contained"
-                        sx={{ width: '300px', height: '50px' }}
-                    >
-                        Faça seu orçamento
-                    </Button>
-                </Grid>
-                <Grid
-                    className="image-container"
-                    sx={{
-                        display: 'flex',
-                        justifyContent: {
-                            xs: 'center',
-                        },
-                        alignItems: 'center',
-                        width: {
-                            xs: '100%',
-                        },
-                    }}
-                >
-                    <Image className="image" src={heroImage} alt="teste" />
-                </Grid>
-            </Box>
+          </Typography>
+          <Button
+            href={
+              'https://api.whatsapp.com/send?phone=5571988078997&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20minha%20locu%C3%A7%C3%A3o.'
+            }
+            variant="contained"
+            sx={{ width: '300px', height: '50px' }}
+          >
+            Faça seu orçamento
+          </Button>
         </Grid>
-    )
+        <Grid
+          className="image-container"
+          sx={{
+            display: 'flex',
+            justifyContent: {
+              xs: 'center',
+            },
+            alignItems: 'center',
+            width: {
+              xs: '100%',
+            },
+          }}
+        >
+          <Image className="image" src={heroImage} alt="teste" />
+        </Grid>
+      </Box>
+    </Grid>
+  );
 }
